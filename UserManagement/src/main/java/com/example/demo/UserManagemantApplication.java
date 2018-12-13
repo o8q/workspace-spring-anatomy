82a73321c5497342162a6774200441ccaf17121e
o8q <o8qqqq@gmail.com>
2018-12-13 22:41:07 +0900

    initialize. add service User Management System.

---
+++ UserManagement/src/main/java/com/example/demo/UserManagemantApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagemantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagemantApplication.class, args);
+	}
+}
---
+++ UserManagement/src/main/resources/db/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS employee(
+id INT PRIMARY KEY,
+name VARCHAR(50),
+age INT
+);
